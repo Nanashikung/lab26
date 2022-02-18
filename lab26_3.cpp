48d64497bcba5f8ed9409ec5c690173aa91f19d7
Nanashikung <nata.station@hotmail.com>
2022-02-18 19:41:36 +0700

    lab26_3.cpp complete

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-//Write function count() here.
+#include<set>
+int count(int data[],unsigned int N){
+	set<int> mySet;
+	for(unsigned int i=0;i < N;i++){
+		mySet.insert(data[i]);
+	}
+	return mySet.size();
+
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
